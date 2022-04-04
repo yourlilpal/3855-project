6bade7d2dec2b780277755d934649e146c21d542
Gorden Pal <gpal@my.bcit.ca>
2022-04-04 15:46:25 -0400

    lab9 content

--- Audit/app.py
+++ Audit/app.py
@@ -70,11 +70,10 @@ def get_user_password(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
-    app = connexion.FlaskApp(__name__, specification_dir='')
-    CORS(app.app)
-    app.app.config['CORS_HEADERS'] = 'Content-Type'
     app.run(port=8110)
--- Processing/app.py
+++ Processing/app.py
@@ -155,13 +155,12 @@ def get_stats():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     # run our standalone gevent server
     init_scheduler()
-    app = connexion.FlaskApp(__name__, specification_dir='')
-    CORS(app.app)
-    app.app.config['CORS_HEADERS'] = 'Content-Type'
     app.run(port=8100, use_reloader=False)
     # app.run(port=8100)
