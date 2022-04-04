33b8702e78fe87d306b7cae00de811462a7a7873
Gorden Pal <gpal@my.bcit.ca>
2022-04-04 19:15:32 -0400

    lab9 content

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -6,7 +6,7 @@ import AppStats from './components/AppStats'
 
 function App() {
 
-    const endpoints = ["password_user", "user_password"]
+    const endpoints = ["user/password", "login/user"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
