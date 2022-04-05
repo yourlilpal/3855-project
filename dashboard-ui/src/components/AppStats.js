12c12d517e5ea7b89f114d5888e99da04088f192
Gorden Pal <gpal@my.bcit.ca>
2022-04-04 22:54:29 -0400

    local

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -8,7 +8,7 @@ export default function AppStats() {
 
 	const getStats = () => {
 	
-        fetch(`http://kafka-3855.eastus2.cloudapp.azure.com:8100/stats`)
+        fetch(`http://localhost:8100/stats`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Stats")
